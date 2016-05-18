4f14e31f59669491a8a8fd810c904fa4aef0e057
Christina Reilly <creilly2192@gmail.com>
2016-05-18 08:48:21 -0500

    add button controls and style

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -1,6 +1,7 @@
 var React = require('react');
 var Clock = require('Clock');
 var CountdownForm = require('CountdownForm');
+var Controls = require('Controls');
 
 var Countdown = React.createClass({
   getInitialState: function () {
@@ -16,6 +17,12 @@ var Countdown = React.createClass({
         case 'started':
           this.startTimer();
           break;
+        case 'stopped':
+          this.setState({count: 0});
+        case 'paused':
+          clearInterval(this.timer)
+          this.timer = undefined;
+          break;
       }
     }
   },
@@ -26,7 +33,7 @@ var Countdown = React.createClass({
         count: newCount >= 0 ? newCount : 0
       });
 
-      
+
     }, 1000);
   },
   handleSetCountdown: function(seconds) {
@@ -35,12 +42,23 @@ var Countdown = React.createClass({
       countdownStatus: 'started'
     });
   },
+  handleStatusChange: function(newStatus) {
+    this.setState({countdownStatus: newStatus});
+  },
   render: function () {
-    var {count} = this.state;
+    var {count, countdownStatus} = this.state;
+    var renderControlArea = () => {
+      if(countdownStatus !== 'stopped') {
+        return <Controls countdownStatus={countdownStatus} onStatusChange={this.handleStatusChange}/>
+      } else {
+        return <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+      }
+    };
+
     return (
       <div className="row">
         <Clock totalSeconds={count}/>
-        <CountdownForm onSetCountdown={this.handleSetCountdown}/>
+        {renderControlArea()}
       </div>
     );
   }
--- webpack.config.js
+++ webpack.config.js
@@ -29,7 +29,8 @@ module.exports = {
       Timer: 'app/components/Timer.jsx',
       Countdown: 'app/components/Countdown.jsx',
       Clock: 'app/components/Clock.jsx',
-      CountdownForm: 'app/components/CountdownForm.jsx'
+      CountdownForm: 'app/components/CountdownForm.jsx',
+      Controls: 'app/components/Controls.jsx'
     },
     //file extensions to process
     extensions: ['', '.js', '.jsx']
