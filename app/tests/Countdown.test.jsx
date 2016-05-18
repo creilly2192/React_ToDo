e45bd1ff47d6dddc4ee41b372e9d685241f003ba
Christina Reilly <creilly2192@gmail.com>
2016-05-18 09:45:52 -0500

    finalize tests & and add page title

--- app/tests/Countdown.test.jsx
+++ app/tests/Countdown.test.jsx
@@ -37,6 +37,30 @@ describe('Countdown', () => {
 
     });
 
+    it('should pause countdown on pause status', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('paused');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(3);
+        expect(countdown.state.countdownStatus).toBe('paused');
+        done();
+      }, 1001);
+    });
+
+    it('should stop countdown on stop status', () => {
+      var countdown = TestUtils.renderIntoDocument(<Countdown/>);
+      countdown.handleSetCountdown(3);
+      countdown.handleStatusChange('stopped');
+
+      setTimeout(() => {
+        expect(countdown.state.count).toBe(0);
+        expect(countdown.state.countdownStatus).toBe('stopped');
+        done();
+      }, 1001);
+    });
+
   });
 
 
