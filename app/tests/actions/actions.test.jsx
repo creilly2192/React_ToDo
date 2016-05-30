bed8ebe34527e00b93594f5b1557fa2f1b00c6e0
Christina Reilly <creilly2192@gmail.com>
2016-05-30 11:34:05 -0500

    add show completed and search text reducers

--- app/tests/actions/actions.test.jsx
+++ app/tests/actions/actions.test.jsx
@@ -5,10 +5,18 @@ describe('Actions', () => {
   it('should generate search text action', () => {
     var action = {
       type: 'SET_SEARCH_TEXT',
-      searchText: 'some search text'
+      searchText: 'Some search text'
     };
-
     var res = actions.setSearchText(action.searchText);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate toggle show completed action', () => {
+    var action = {
+      type: 'TOGGLE_SHOW_COMPLETED'
+    };
+    var res = actions.toggleShowCompleted();
 
     expect(res).toEqual(action);
   });
@@ -16,10 +24,19 @@ describe('Actions', () => {
   it('should generate add todo action', () => {
     var action = {
       type: 'ADD_TODO',
-      text: 'thing todo'
+      text: 'Thing to do'
     };
-
     var res = actions.addTodo(action.text);
+
+    expect(res).toEqual(action);
+  });
+
+  it('should generate toggle todo action', () => {
+    var action = {
+      type: 'TOGGLE_TODO',
+      id: '123'
+    };
+    var res = actions.toggleTodo(action.id);
 
     expect(res).toEqual(action);
   });
--- webpack.config.js
+++ webpack.config.js
@@ -30,7 +30,8 @@ module.exports = {
     ],
     alias: {
       applicationStyles: 'app/styles/app.scss',
-      actions: 'app/actions/actions.jsx'
+      actions: 'app/actions/actions.jsx',
+      reducers: 'app/reducers/reducers.jsx'
     },
     //file extensions to process
     extensions: ['', '.js', '.jsx']
